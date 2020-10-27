fda3ed60f936cb5adcf312f51b4b224409dbb908
Dustin Craig <dustincraig@Dustins-MacBook-Pro.local>
2020-10-27 10:59:01 -0400

    updates

--- data.py
+++ data.py
@@ -5,6 +5,12 @@ import csv
 import time
 import datetime
 
+# All data is collected based on past \query_size\ submissions/comments
+########## DATASET STRUCTURE #############
+# subreddit_name | number_of_posts_removed_by_mod | submissions_containing_toxic_words | comments_containing_toxic_words | nfsw_submissions | locked_submissions | original_submissions | subreddit_subscribers | repeat_
+# aww            | 10                             | 2                                  | 10
+##########################################
+
 # Size of reddit query
 query_size = 1
 
@@ -19,21 +25,28 @@ toxic_words_file_path = './toxicwords.txt'
 # Subreddits to check
 subreddits = [
     'news',
-    'csgo',
-    'aww',
+    # 'csgo',
+    # 'aww',
 ]
 
 
-def getSubmissionData(sub):
+def getSubmissionData(sub, query_params={}):
     url = base_url + 'submission/?subreddit=' + \
         sub + '&size=' + str(query_size)
+    for param in query_params:
+        ''' Add all requested query params to url '''
+        url = url + '&' + param + '=' + str(query_params[param])
+    print('URL ', url)
     r = requests.get(url)
     data = json.loads(r.text)
     return data['data']
 
 
-def getCommentData(sub):
+def getCommentData(sub, query_params=[]):
     url = base_url + 'comment/?subreddit=' + sub + '&size=' + str(query_size)
+    for param in query_params:
+        ''' Add all requested query params to url '''
+        url = url + '&' + param + '=' + str(query_params[param])
     r = requests.get(url)
     data = json.loads(r.text)
     return data['data']
@@ -47,5 +60,5 @@ if __name__ == '__main__':
     # For every subreddit, search the last 400 submissions/comments for our toxic words
     for subreddit in subreddits:
         submission_data = getSubmissionData(subreddit)
-        comment_data = getCommentData(subreddit)
-        print(comment_data)
+        # comment_data = getCommentData(subreddit)
+        print(submission_data)
